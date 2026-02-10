a1d3afab8a6537cb35fccdfd3450b2dac5348794
Mohamed_Hany <57901178+mohamed-h-eng@users.noreply.github.com>
2026-02-10 20:46:35 +0200

    Add files via upload

---
+++ product_inventory/app.js
@@ -0,0 +1,30 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+const express = require("express") 
+const app = express()
+app.use(express.json())
+const port = process.env.PORT
+
+
+async function DBconnection(){
+    try{
+        await mongoose.connect(process.env.DBURL)
+        console.log("db connected");
+    }catch(error){
+        console.log("db disconnected")
+    }
+}
+DBconnection()
+
+app.get("/",(req,res)=>{
+    try{
+        res.send("welcome to product inventory")
+        console.log("success reading product inventory")
+    }catch(error){
+        console.log("error reading product inventory")
+    }
+})
+
+app.listen(port,()=>{
+    console.log("server running ...")
+})
