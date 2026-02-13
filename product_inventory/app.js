b4b5ff3ffb0bf63775958ac827a1a41b871f576b
Mohamed_Hany <57901178+mohamed-h-eng@users.noreply.github.com>
2026-02-13 19:49:26 +0200

    Add files via upload

--- product_inventory/app.js
+++ product_inventory/app.js
@@ -25,6 +25,35 @@ app.get("/",(req,res)=>{
     }
 })
 
+const Product = require("./models/Product")
+app.post("/api/products",async (req,res)=>{
+    try{
+        const product = await Product.create(req.body);
+        res.json({
+            msg:"product read successfully",
+            data:product
+        })
+        console.log("success creating product in inventory")
+    }catch(error){
+        console.log("error creating product in inventory")
+    }
+})
+
+app.get("/api/products",async (req,res)=>{
+    try{
+        let product = await Product.find();
+        if (req.query["category"]){
+            product = await Product.find(req.query);
+        }
+        res.json({
+            msg:"products read successfully",
+            data:product
+        })
+        console.log("success reading product inventory")
+    }catch(error){
+        console.log("error reading product inventory")
+    }
+})
 app.listen(port,()=>{
     console.log("server running ...")
 })
